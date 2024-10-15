68aee63a7c02f680d46bc6b4ba0523dce8b592a1
Jayd <holdsworthjayd@gmail.com>
2024-10-15 11:41:04 +0100

    feat: style not found page

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,12 +1,23 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function NotFound() {
+  const router = useRouter();
+
+  const handleButtonPress = () => {
+    router.replace("/tabs/map");
+  };
+
   return (
     <View className="flex-1 justify-center items-center bg-background">
       <Text className="text-2xl font-bold text-text font-montserrat">
         Oops! Page not found.
       </Text>
+      <TouchableOpacity className="bg-buttonbg border border-accent-2 w-5/6 p-3 rounded-3xl mb-4 items-center"
+      onPress={handleButtonPress}>
+      <Text className="text-text font-montserrat-alt text-xl">Go Back...</Text>
+      </TouchableOpacity>
     </View>
   );
 }
