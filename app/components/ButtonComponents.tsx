97357eb873122fdba06ccfafdf469ab003de6f3f
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2024-10-29 05:04:51 +0000

    feat: boss button looks a bit prettier + cleaned up unecessary code

--- app/components/ButtonComponents.tsx
+++ app/components/ButtonComponents.tsx
@@ -20,21 +20,24 @@ export const TrickButton: React.FC<ButtonProps> = ({
   onPress,
   isCompleted = 0,
 }) => {
-  let iconSize = 32;
+  const iconSize: number = 32;
 
   const truncatedName = name.length > 10 ? name.slice(0, 10) + "..." : name;
-  const textSize = name.length < 8 ? "text-sm" : "text-xs";
+  const textSize = name.length < 8 ? "text-sm" : "text-xs -mb-1";
+  const viewSize = name.length < 8 ? "" : "-mt-10";
 
   return (
     <Pressable
-      className={`relative w-[80px] h-[100px] rounded-2xl bg-buttonbg shadow border ${
+      className={`relative w-[80px] h-[100px] rounded-2xl bg-buttonbg shadow-md border ${
         isCompleted == 0 ? "border-red shadow-red" : ""
       } ${isCompleted == 1 ? "border-yellow shadow-yellow" : ""} ${
         isCompleted == 2 ? "border-green shadow-green" : ""
       }`}
       onPress={() => onPress(id)}
     >
-      <View className="w-full h-full flex-1 items-center -mt-6 justify-center">
+      <View
+        className={`w-full h-full flex-1 items-center -mt-6 justify-center ${viewSize} `}
+      >
         <SkateboardIcon width={iconSize} height={iconSize} />
       </View>
       <View className="absolute bottom-4 w-full px-2">
@@ -54,13 +57,11 @@ export const InfoButton: React.FC<ButtonProps> = ({
   onPress,
   isCompletedInfo,
 }) => {
-  let iconSize = 35;
+  const iconSize: number = 35;
   return (
     <Pressable
-      className={`p-2 rounded-2xl bg-buttonbg items-center justify-center w-[75] shadow-inner border h-[100] ${
-        isCompletedInfo == false
-          ? "border-red shadow-red"
-          : "border-green shadow-green"
+      className={`p-2 rounded-2xl bg-buttonbg items-center justify-center w-[75] border h-[100] ${
+        isCompletedInfo == false ? "border-red" : "border-green"
       }`}
       onPress={() => onPress(id)}
     >
@@ -78,25 +79,33 @@ export const BossButton: React.FC<ButtonProps> = ({
   onPress,
   isCompleted = 0,
 }) => {
-  let iconSize = 80;
+  const iconSize: number = 66;
+  const iconLRSize: number = 14;
   return (
     <Pressable
-      className={`p-4 rounded-2xl bg-buttonbg items-center justify-center w-5/6 shadow-inner border h-[140] ${
-        isCompleted == 0 ? "border-red shadow-red shadow-inner-red" : ""
+      className={`p-4 rounded-2xl bg-buttonbg items-center justify-center w-5/6 shadow-xl border h-[120] py-6 ${
+        isCompleted == 0 ? "border-red shadow-red" : ""
       } ${isCompleted == 1 ? "border-yellow shadow-yellow" : ""} ${
         isCompleted == 2 ? "border-green shadow-green" : ""
       }`}
       onPress={() => onPress(id)}
     >
-      <View className="flex-row justify-between items-center w-full mb-1">
-        <SkaterLeftIcon width={iconSize} height={iconSize} />
-        <BossSkullIcon width={iconSize + 10} height={iconSize + 10} />
-        <SkaterRightIcon width={iconSize} height={iconSize} />
+      <View className="flex-row justify-between items-center w-full">
+        <SkaterLeftIcon
+          width={iconSize + iconLRSize}
+          height={iconSize + iconLRSize}
+          className=""
+        />
+        <BossSkullIcon width={iconSize} height={iconSize} />
+        <SkaterRightIcon
+          width={iconSize + iconLRSize}
+          height={iconSize + iconLRSize}
+        />
       </View>
       <Text
-        className={`text-text font-montserrat-alt-bold text-base text-center ${
-          isCompleted ? "text-accent" : ""
-        }`}
+        className={
+          "text-text font-montserrat-alt-semibold text-base text-center tracking-[6px] uppercase -mt-2"
+        }
       >
         {name}
       </Text>
