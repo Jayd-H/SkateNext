a445aea9b19e740ef9be3e2381129ffdacc45791
Jayden Holdsworth <70723772+Jayd-H@users.noreply.github.com>
2025-01-07 08:10:13 +0000

    chore: unused text import

--- app/components/Onboarding/WelcomeAnimation.tsx
+++ app/components/Onboarding/WelcomeAnimation.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
